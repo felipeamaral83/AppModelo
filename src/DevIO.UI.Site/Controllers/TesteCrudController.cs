0b317e10345d8bf7d800d47c23a56b95eb3693a9
Felipe Amaral de Oliveira <felipeamaral83@yahoo.com.br>
2020-10-07 15:29:46 -0300

    Desenvolvimento do curso.

---
+++ src/DevIO.UI.Site/Controllers/TesteCrudController.cs
@@ -0,0 +1,44 @@
+﻿using DevIO.UI.Site.Data;
+using DevIO.UI.Site.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace DevIO.UI.Site.Controllers
+{
+    public class TesteCrudController : Controller
+    {
+        private readonly MeuDbContext _contexto;
+
+        public TesteCrudController(MeuDbContext contexto)
+        {
+            _contexto = contexto;
+        }
+        
+        public IActionResult Index()
+        {
+            var aluno = new Aluno
+            {
+                Nome = "Felipe",
+                Email = "felipeamaral83@yahoo.com.br",
+                DataNascimento = DateTime.Now
+            };
+
+            _contexto.Alunos.Add(aluno);
+            _contexto.SaveChanges();
+
+            var aluno2 = _contexto.Alunos.Find(aluno.Id);
+            var aluno3 = _contexto.Alunos.FirstOrDefault(x => x.Email == "felipeamaral83@yahoo.com.br");
+            var aluno4 = _contexto.Alunos.Where(x => x.Nome == "Felipe");
+
+            aluno.Nome = "João";
+            _contexto.Alunos.Update(aluno);
+            _contexto.SaveChanges();
+
+            _contexto.Alunos.Remove(aluno);
+            _contexto.SaveChanges();
+
+            return View("_Layout");
+        }
+    }
+}
--- src/DevIO.UI.Site/Startup.cs
+++ src/DevIO.UI.Site/Startup.cs
@@ -1,7 +1,13 @@
+using DevIO.UI.Site.Data;
+using DevIO.UI.Site.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace DevIO.UI.Site
 {
@@ -9,9 +15,36 @@ namespace DevIO.UI.Site
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.AreaViewLocationFormats.Clear();
+                options.AreaViewLocationFormats.Add("/Modulos/{2}/Views/{1}/{0}.cshtml");
+                options.AreaViewLocationFormats.Add("/Modulos/{2}/Views/Shared/{0}.cshtml");
+                options.AreaViewLocationFormats.Add("/Views/Shared/{0}.cshtml");
+            });
+
+            services.AddDbContext<MeuDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("MeuDbContext")));
+
             services.AddControllersWithViews();
+
+            services.AddTransient<IPedidoRepository, PedidoRepository>();
+
+            services.AddTransient<IOperacaoTransient, Operacao>();
+            services.AddScoped<IOperacaoScoped, Operacao>();
+            services.AddSingleton<IOperacaoSingleton, Operacao>();
+            services.AddSingleton<IOperacaoSingletonInstance>(new Operacao(Guid.Empty));
+
+            services.AddTransient<OperacaoService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,6 +60,9 @@ namespace DevIO.UI.Site
 
             app.UseEndpoints(endpoints =>
             {
+                //endpoints.MapControllerRoute("areas", "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapAreaControllerRoute("AreaProdutos", "Produtos", "Produtos/{controller=Cadastro}/{action=Index}/{id?}");
+                endpoints.MapAreaControllerRoute("AreaVendas", "Vendas", "Vendas/{controller=Pedidos}/{action=Index}/{id?}");
                 endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
